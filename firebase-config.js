7487e6c603f6812d72b2f6c8461661bdd7c0d738
DogLover-0616 <airisuto0616@gmail.com>
2025-09-16 14:45:41 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+// ここに自分のFirebaseプロジェクト情報を貼り付けてください
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  // 必要に応じて他の設定も
+};
