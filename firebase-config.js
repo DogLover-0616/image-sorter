35e9b14cfaf64d739ed78a7791164ca1cebaaf0d
DogLover-0616 <airisuto0616@gmail.com>
2025-09-17 10:37:26 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,7 @@
+// ここに自分のFirebaseプロジェクト情報を貼り付けてください
+export const firebaseConfig = {
+  apiKey: "AIzaSyD9D6IE-FdCYOKqVzPpl7E8qK7njnBxqfQ",
+  authDomain: "image-sorter-5a77d.firebaseapp.com",
+  projectId: "image-sorter-5a77d",
+  // 必要に応じて他の設定も
+};
