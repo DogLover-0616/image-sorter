5a854c9d58a32f6d537d8ee03ee0fa2ab4532a3f
imgsort <airisuto0616@gmail.com>
2026-06-12 15:50:20 +0900

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-const _firebaseConfig = {
-  apiKey:            "AIzaSyA_N6mtmZgvCxChjEcJqRGHvKCJw9pWXHE",
-  authDomain:        "sales-management-31979.firebaseapp.com",
-  projectId:         "sales-management-31979",
-  storageBucket:     "sales-management-31979.firebasestorage.app",
-  messagingSenderId: "339482971964",
-  appId:             "1:339482971964:web:3f2551ba94b7a55d361839"
-};
